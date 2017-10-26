3886d3603877f9f6b598906f80087af8fec9db58
clovisHub <clovajob@yahoo.com>
2017-10-25 23:41:14 -0400

    Add Map

--- app/src/main/java/com/example/admin/pineapple/MainActivity.java
+++ app/src/main/java/com/example/admin/pineapple/MainActivity.java
@@ -1,13 +1,116 @@
 package com.example.admin.pineapple;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.admin.pineapple.view.MapsActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btnGo;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    Double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userLocation();
+
+        btnGo = (Button) findViewById(R.id.btn_goId);
+        btnGo.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            if(ContextCompat.checkSelfPermission
+                    (this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10,10,locationListener);
+            }
+
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+
+        intent.putExtra("lat",latitude);
+        intent.putExtra("longit",longitude);
+        startActivity(intent);
+    }
+
+    public void userLocation(){
+
+        locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+
+                Log.i("Location",location.toString());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+
+        };
+
+        //sdk version < 23 version
+        if(Build.VERSION.SDK_INT < 23){
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10,10,locationListener);
+        }
+        else{
+
+            if(ContextCompat.checkSelfPermission
+                    (this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+            }
+            else{
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10,10,locationListener);
+            }
+
+        }
+
     }
 }
