4373e54559125f8046197cefb8b219593585c3a6
clovisHub <clovajob@yahoo.com>
2017-10-26 11:49:52 -0400

    Add user location

--- app/src/main/java/com/example/admin/pineapple/view/MapsActivity.java
+++ app/src/main/java/com/example/admin/pineapple/view/MapsActivity.java
@@ -1,8 +1,19 @@
 package com.example.admin.pineapple.view;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.example.admin.pineapple.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -18,6 +29,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     Double latitude, longitude;
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,34 +40,100 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        Intent intent = getIntent();
-        latitude = intent.getDoubleExtra("lat",0.0);
-        longitude = intent.getDoubleExtra("longit",0.0);
+
+      //  Intent intent = getIntent();
+      //  latitude = intent.getDoubleExtra("lat", 0);
+      //   longitude = intent.getDoubleExtra("longit", 0);
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        if(requestCode == 1){
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                if(ContextCompat.checkSelfPermission
+                        (this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10,0,locationListener);
+
+                }
+
+            }
+        }
+
+    }
+
+    public void userLocation(){
+
+        locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+
+                LatLng atlanta = new LatLng(latitude,longitude);
+                mMap.addMarker(new MarkerOptions()
+                        .position(atlanta)
+                        .title("Your location"))
+                        .setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(atlanta,5));
+
+                Log.i("Location",location.toString());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+
+        };
+
+        //sdk version < 23 version
+        if(Build.VERSION.SDK_INT < 23){
+
+            //  locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,15,0,locationListener);
+        }
+        else{
+
+            if(ContextCompat.checkSelfPermission
+                    (this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+            }
+            else{
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+            }
+
+        }
+
+    }
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+        userLocation();
+
         mMap = googleMap;
         //Setmap type
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
         // Add a marker in Sydney and move the camera
-        LatLng atlanta = new LatLng(latitude,longitude);
-        mMap.addMarker(new MarkerOptions()
-                .position(atlanta)
-                .title("Your location"))
-                .setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(atlanta,5));
+
 
         /*
         LatLng sydney = new LatLng();
